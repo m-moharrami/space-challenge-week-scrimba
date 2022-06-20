7029a59c5fad707ecd258152325ecb0ae492ee0b
m-moharrami <moharram.m98@gmail.com>
2022-06-20 23:32:55 +0430

    add planets data

---
+++ day-1/code.js
@@ -0,0 +1,43 @@
+const solarSystem = document.getElementById("solarSystem");
+const planets = [
+    {
+    "name": "Mercury",
+    "diameter": 4879,
+    "color": "grey"
+    },
+    {
+    "name": "Venus",
+    "diameter": 12104,
+    "color": "yellow"
+    },
+    {
+    "name": "Earth",
+    "diameter": 12742,
+    "color": "green"
+    },
+    {
+    "name": "Mars",
+    "diameter": 6779,
+    "color": "pink"
+    },
+    {
+    "name": "Jupiter",
+    "diameter": 139822,
+    "color": "yellow"
+    },
+    {
+    "name": "Saturn",
+    "diameter": 116464,
+    "color": "orange"
+    },
+    {
+    "name": "Uranus",
+    "diameter": 50724,
+    "color": "grey"
+    },
+    {
+    "name": "Neptune",
+    "diameter": 49244,
+    "color": "blue"
+    }
+];
